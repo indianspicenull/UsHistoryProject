7cbe9d909c2c75c2aa58faf77321f806546163f1
e <49532012+bobdinh139@users.noreply.github.com>
2019-05-28 13:16:15 -0400

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 function fade($ele) {
-    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+    $ele.fadeIn(1000).delay(5000).fadeOut(1000, function() {
         var $next = $(this).next('.quote');
         fade($next.length > 0 ? $next : $(this).parent().children().first());
    });
